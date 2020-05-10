b205fd83a3512c3641eb7d1ee59e7b0713f99287
Jack Honour <JaxkDev@gmail.com>
2020-05-10 09:55:16 +0100

    Fire...

---
+++ src/JaxkDev/TestPlugin/HuhCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JaxkDev\TestPlugin;
+
+use pocketmine\command\Command;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\plugin\Plugin;
+
+class HuhCommand extends Command implements PluginIdentifiableCommand{
+
+	private $plugin;
+
+	public function __construct(string $name, $plugin){
+		parent::__construct($name);
+		$this->plugin = $plugin;
+		$this->setUsage("/huh <What> !");
+		$this->setDescription("Huh whats this...");
+		$this->setPermission("huh.command.huh");
+	}
+
+	public function execute(CommandSender $sender, string $commandLabel, array $args){
+    $sender->sendMesssage("Huh");
+    return true;
+  }
+
+	public function getPlugin() : Plugin{
+		return $this->plugin;
+	}
+}
