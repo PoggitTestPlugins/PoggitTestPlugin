7d0466b52e7469571d0a72ccb82a7ad09bfd5cb7
Jack Honour <JaxkDev@gmail.com>
2020-05-10 10:12:46 +0100

    Update Main.php

--- src/JaxkDev/TestPlugin/Main.php
+++ src/JaxkDev/TestPlugin/Main.php
@@ -7,7 +7,7 @@ class Main extends PluginBase{
 	public function onEnable(){
 		$this->getLogger()->info("Enabled.");
 		//No changes huh...
-		$this->getServer()->getCommandMap()->register("huh", new HuhCommand($this));
+		$this->getServer()->getCommandMap()->register("huh", new HuhCommand("huhCommand",$this));
 	}
 	
 	public function onDisable(){
